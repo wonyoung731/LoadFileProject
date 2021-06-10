2d69f806c00d070b0ad0655fbbfe301f2041d23a
wonyoung731 <85541037+wonyoung731@users.noreply.github.com>
2021-06-10 17:27:37 +0900

    Add files via upload
    Success to show folder path

---
+++ HelloWPFApp/HelloWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace HelloWPFApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.button.Click += button_Click;
+        }
+        
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog folderDialog = new CommonOpenFileDialog()
+            {
+                InitialDirectory = "",
+                IsFolderPicker = true
+            };
+            if(folderDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                this.textBox.Text = folderDialog.FileName;
+            }
+        }
+        
+    }
+}
